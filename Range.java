336539eae1575bc77a20dc49741b8e3622e2a9ca
Alex Cho <acho20@stuy.edu>
2020-12-06 11:05:20 -0500

    copied Range.java, created constructor

---
+++ Range.java
@@ -0,0 +1,16 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence{
+  private int start,end,current;
+  public Range(int start,  int end){
+    this.start = start;
+    this.end = end;
+    current = 0;
+  }
+  public void reset(){  }
+  public int length(){  }
+  public boolean hasNext(){  }
+
+  //@throws NoSuchElementException
+  public int next(){    }
+
+}
